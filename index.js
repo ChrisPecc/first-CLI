0a216fbbe9e7aab2dd316d5a5f55cb20594c7aa1
ChrisPecc <pecceu.christophe@gmail.com>
2020-09-08 14:12:00 +0200

    v1.0.7

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const axios = require('axios').default;
 const { getCode, getName } = require('country-list');
 const chalk = require('chalk');
 
-let myArgs = process.argv.slice(2);
+const myArgs = process.argv.slice(2);
 // console.log(myArgs);
 
-let country = myArgs[0];
-let countrycode = getCode(country);
+const country = myArgs[0];
+const countrycode = getCode(country);
 if (countrycode == undefined){
     console.log("This is not a proper country name or the country is not supported by the API. \nCheck at https://date.nager.at/Home/Countries to see the list of supported countries.")
 }
 
 else {
     // console.log(countrycode);
-    let date = new Date();
-    let year = date.getFullYear();
+    const date = new Date();
+    const year = date.getFullYear();
     // console.log(year);
     
     const url = String("https://date.nager.at/Api/v2/PublicHolidays/"+year+"/"+countrycode);
